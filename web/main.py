45228cfa056599dae3b97228a79f72df450a1651
Thomas Corcoran <chasecorcoran@gmail.com>
2021-03-13 22:08:20 -0800

    add video streaming

--- web/main.py
+++ web/main.py
@@ -4,12 +4,26 @@ from fastapi.staticfiles import StaticFiles
 from asyncio import sleep
 from random import randint
 from pathlib import  Path
+import io
 import json
+import base64
 #from connected_garden.read_sensors_async import read_sensors
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 html = Path("templates/index.html").read_text()
+
+vid_template = Path("templates/video.html").read_text()
+import logging
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+try:
+    import picamera
+    camera = picamera.PiCamera()
+    camera.start_preview()
+    camera.resolution = (640,480)
+except ImportError:
+    logger.warn("Picamera is not installed!")
 
 async def get_sensor_values():
     await sleep(1)
@@ -27,9 +41,20 @@ async def get_sensor_values():
 async def get():
     return HTMLResponse(html)
 
+@app.get("/stream")
+async def get():
+    return HTMLResponse(vid_template)
 
 @app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
     while True:
         await websocket.send_json(await get_sensor_values())
+
+@app.websocket("/video-stream")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    sio = io.BytesIO()
+    while True:
+        camera.capture(sio, "jpeg", use_video_port=True)
+        await websocket.send_bytes(base64.b64encode(sio.getvalue()))
